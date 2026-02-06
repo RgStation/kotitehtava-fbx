a31ef75d9e86c28c0b069e820d45a96b60254dcc
Ronja Gröhn <ronja.grohn@student.lab.fi>
2026-02-07 00:55:57 +0200

    Adjust scale of loaded FBX object

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ function init() {
     loader.load(
         "../fbx/DoughNut_FBX.fbx", // polku js-kansiosta fbxiin
         function(object) {
-            object.scale.set(0.005, 0.005, 0.005);
+            object.scale.set(0.0005, 0.0005, 0.0005);
             object.position.set(0, 0, -0.3);
             scene.add(object);
             console.log("DoughNut FBX loaded:", object);
